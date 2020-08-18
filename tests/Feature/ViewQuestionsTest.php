d739bb2cd44db26501ec6a02835c01c235dd6605
vagrant <vagrant@homestead>
2020-08-18 17:41:26 +0800

    rename tests

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -7,8 +7,20 @@ use Tests\TestCase;
 
 class ViewQuestionsTest extends TestCase
 {
-    
-    public function testUserCanViewQuestions()
+
+//    public function testUserCanViewQuestions()
+//    {
+//        // 0. 抛出异常
+//        $this->withoutExceptionHandling();
+//        // 1.假设/questions 路由存在 2. 访问链接 /questions
+//        $test = $this->get('/questions');
+//        // 3. 正常返回 200
+//        $test->assertStatus(200);
+//    }
+
+    // 推荐使用这种，具有更佳的可读性
+    /** @test **/
+    public function user_can_view_questions()
     {
         // 0. 抛出异常
         $this->withoutExceptionHandling();
