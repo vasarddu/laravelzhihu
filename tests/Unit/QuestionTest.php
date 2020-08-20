aa3dac100a673d203fa43b9dd47a871754658b65
vasar <vasarddu@gmail.com>
2020-08-20 12:24:30 +0800

    post answer

--- tests/Unit/QuestionTest.php
+++ tests/Unit/QuestionTest.php
@@ -13,6 +13,8 @@ class QuestionTest extends TestCase
     use RefreshDatabase;
 
     /** @test **/
+    // 单元测试-作用是写出简洁无错的代码，代码非常少，而且相对独立的一部分代码来测试，大部分单元测试针对单个方法进行的。
+    // 单元测试由功能测试驱动，而且更接近于真正的代码。
     public function a_question_has_many_answers()
     {
         $question = factory(Question::class)->create();
